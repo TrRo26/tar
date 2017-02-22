5df9c02d986430d39c9a54089b23f6ecd2814567
Travis Roy <travis-mac@Travis-iMac.hsd1.il.comcast.net>
2017-02-21 23:58:22 -0600

    Add background image to projects

--- tar-site/app/assets/javascripts/components/projects.jsx
+++ tar-site/app/assets/javascripts/components/projects.jsx
@@ -2,17 +2,17 @@
 var Projects = React.createClass({
 	render: function() {
 		return (
-			<div className='category'>
-				<h1>
-      		<div className='cat-title'>p</div>
-      		<div className='cat-title'>r</div>
-      		<div className='cat-title'>o</div>
-      		<div className='cat-title'>j</div>
-      		<div className='cat-title'>e</div>
-      		<div className='cat-title'>c</div>
-      		<div className='cat-title'>t</div>
-      		<div className='cat-title'>s</div>
-      	</h1>
+			<div className='category' id='proj-image'>
+		      <h1>
+        		<div className='cat-title'>p</div>
+        		<div className='cat-title'>r</div>
+        		<div className='cat-title'>o</div>
+        		<div className='cat-title'>j</div>
+        		<div className='cat-title'>e</div>
+        		<div className='cat-title'>c</div>
+        		<div className='cat-title'>t</div>
+        		<div className='cat-title'>s</div>
+      	  </h1>
 			</div>
 		)
 	}
