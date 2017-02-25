509466fbedfa198ecba0554ec662d6b73a2fa4c0
Travis Roy <travis-mac@Travis-iMac.local>
2017-02-25 16:22:41 -0600

    Add standard and fix some of it's rediculous complaints

--- tar-site/app/assets/javascripts/components/projects.jsx
+++ tar-site/app/assets/javascripts/components/projects.jsx
@@ -1,3 +1,5 @@
+var React = React.require('react')
+var ReactDOM = React.require('react-dom')
 
 var Projects = React.createClass({
 
@@ -5,17 +7,16 @@ var Projects = React.createClass({
 
   // },
 
+  handleClick: function () {
+    var projHideShow = 'proj-image'
 
-  handleClick: function() {
-    var projHideShow = 'proj-image';
-
-    if(projHideShow == 'proj-image') {
+    if (projHideShow === 'proj-image') {
       let projHideShow = 'proj-no-image'
-    } 
+    }
     // projHideShow == ('proj-image' ? 'proj-no-image' : 'proj-image')
   },
 
-	render: function() {
+	render: function () {
 
     var projHideShow = 'proj-image'
     // var projShow = 'proj-image';
@@ -23,29 +24,26 @@ var Projects = React.createClass({
 
 		return (
 			<div className='category' onClick={this.handleClick} id={projHideShow}>
-		      <h1>
-        		<div className='cat-title'>p</div>
-        		<div className='cat-title'>r</div>
-        		<div className='cat-title'>o</div>
-        		<div className='cat-title'>j</div>
-        		<div className='cat-title'>e</div>
-        		<div className='cat-title'>c</div>
-        		<div className='cat-title'>t</div>
-        		<div className='cat-title'>s</div>
-      	  </h1>
+          <h1>
+            <div className='cat-title'>p</div>
+            <div className='cat-title'>r</div>
+            <div className='cat-title'>o</div>
+            <div className='cat-title'>j</div>
+            <div className='cat-title'>e</div>
+            <div className='cat-title'>c</div>
+            <div className='cat-title'>t</div>
+            <div className='cat-title'>s</div>
+          </h1>
 			</div>
 		)
-	},
+	}
 
-});
+})
 
 // ReactDOM.render(
 //     <Projects />,
 //     document.getElementById('cat-wrapper')
 // )
-
-
-
 
 // class Button extends React.Component {
 //     constructor(props) {
