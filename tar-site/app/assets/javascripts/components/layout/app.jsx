3c4b907c21d02e96bbc33dbfcdbc47603b4a54d6
Travis Roy <travis-mac@Travis-iMac.local>
2017-03-16 17:45:56 -0500

    Complete failure at attempting to use ReactRouter with rails

--- tar-site/app/assets/javascripts/components/layout/app.jsx
+++ tar-site/app/assets/javascripts/components/layout/app.jsx
@@ -1,21 +1,20 @@
-// import { browserHistory } from 'react-router'
+// CANNOT GET RAILS TO RECOGNIZE REACT-ROUTER - TREATS IT AS IT DOESN'T EXIST
 
 var App = React.createClass({
 	
 	render() {
 		return(
-			<ReactRouter.Router>
-				<ReactRouter.Route path='/' component={App}>
-					<ReactRouter.IndexRoute component={Categories} />
-					<ReactRouter.Route path='/about' component={About} />
-					<ReactRouter.Route path='/projects' component={Projects} />
-					<ReactRouter.Route path='/blog' component={Blog} />
-				</ReactRouter.Route>
+			<div>
+			<h2>FUCK THIS GARBAGE</h2>
+			<ReactRouter.Router history={hashHistory}>
+				<ReactRouter.Route path='/categories' component={Categories} />
 			</ReactRouter.Router>
+			</div>
 		)
 	}
 })
 
+// ONLY THE FOLLOWING ON THE PAGE CORRECTLY RENDERS Categories 
 			// <div>
 			// 	<Categories />
 			// </div>
