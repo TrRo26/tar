f6d0dcfc8a82b789688db3c4236cc452141baca4
TrRo26 <travis.roy26@gmail.com>
2017-06-06 16:33:40 -0500

    Add basic ajax navigation

--- tar-site/app/controllers/projects_controller.rb
+++ tar-site/app/controllers/projects_controller.rb
@@ -2,13 +2,7 @@ class ProjectsController < ApplicationController
 	layout "application"
 
 	def index
-		p "PROJ INDEX"
-		p params
 
-		respond_to do |f|
-			f.js
-		end
-		# render js: "alert('AHHHHH')"
 	end
 
 	def show
