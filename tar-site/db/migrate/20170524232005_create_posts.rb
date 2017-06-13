79488ebccff42e942b6d248134d85822a7204f59
TrRo26 <travis.roy26@gmail.com>
2017-06-12 22:19:15 -0500

    Add posts support and styling

--- tar-site/db/migrate/20170524232005_create_posts.rb
+++ tar-site/db/migrate/20170524232005_create_posts.rb
@@ -1,11 +1,12 @@
 class CreatePosts < ActiveRecord::Migration[5.0]
   def change
     create_table :posts do |t|
-    	t.string :title, null: false
+    	t.string :title
     	t.string :sub_title
+      t.string :img_path
      	t.string :author
     	t.text :tags, array: true, default: []
-    	t.string :body, null: false
+    	t.text :body
 
       t.timestamps
     end
