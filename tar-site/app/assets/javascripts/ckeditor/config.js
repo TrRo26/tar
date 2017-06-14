c91095035972b7ae0b4d55f05cea56435513c5eb
TrRo26 <travis.roy26@gmail.com>
2017-06-14 13:33:39 -0500

    Add CKEditor file bloat

--- tar-site/app/assets/javascripts/ckeditor/config.js
+++ tar-site/app/assets/javascripts/ckeditor/config.js
@@ -1,12 +1,19 @@
 CKEDITOR.editorConfig = function(config) {
-  config.language = 'en';
-  config.width = '700';
+  config.language = "en";
+  config.width = "700";
+  config.height = "400";
   config.filebrowserBrowseUrl = "/ckeditor/attachment_files";
   config.filebrowserImageBrowseLinkUrl = "/ckeditor/pictures";
   config.filebrowserImageBrowseUrl = "/ckeditor/pictures";
   config.filebrowserImageUploadUrl = "/ckeditor/pictures";
   config.filebrowserUploadUrl = "/ckeditor/attachment_files";
-
+  config.extraPlugins = 'panel';
+  config.extraPlugins = 'floatpanel';
+  // config.extraPlugins = 'listblock';
+  // config.extraPlugins = 'button';
+  config.extraPlugins = 'richcombo';
+  config.extraPlugins = "lineheight";
+  config.line_height = "1px;1.1px;1.2px;1.3px;1.4px;1.5px;1.6px;1.7px;1.8px;1.9px;2px";
   config.toolbar_Pure = [
     '/', {
       name: 'basicstyles',
--- tar-site/config/initializers/ckeditor.rb
+++ tar-site/config/initializers/ckeditor.rb
@@ -6,6 +6,15 @@ Ckeditor.setup do |config|
   # available as additional gems.
   require 'ckeditor/orm/active_record'
 
+  # Handle custom addons
+  # assets_root =  Rails.root.join('app','assets','javascripts')
+  # ckeditor_plugins_root = assets_root.join('ckeditor','plugins')
+  # %w(openlink sourcedialog).each do |ckeditor_plugin|
+  #   Ckeditor.assets += Dir[ckeditor_plugins_root.join(ckeditor_plugin, '**', '*.js')].map do |x|
+  #     x.sub(assets_root.to_path, '').sub(/^\/+/, '')
+  #   end
+  # end
+
   # Allowed image file types for upload.
   # Set to nil or [] (empty array) for all file types
   # By default: %w(jpg jpeg png gif tiff)
